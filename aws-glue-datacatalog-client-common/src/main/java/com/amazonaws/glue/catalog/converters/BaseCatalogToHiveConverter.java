72a63d6da8da89150f71fb182bab796f0fa20121
Vamsi <vamsi@dremio.com>
2022-02-24 08:33:24 +0530

    DX-41818: handling null serde info coming from Glue iceberg tables
    Change-Id: Ie38c3ef6340f2c0ff26b67b1d3bcf99de94ff541

--- aws-glue-datacatalog-client-common/src/main/java/com/amazonaws/glue/catalog/converters/BaseCatalogToHiveConverter.java
+++ aws-glue-datacatalog-client-common/src/main/java/com/amazonaws/glue/catalog/converters/BaseCatalogToHiveConverter.java
@@ -256,6 +256,12 @@ public class BaseCatalogToHiveConverter implements CatalogToHiveConverter {
 
   public SerDeInfo convertSerDeInfo(com.amazonaws.services.glue.model.SerDeInfo catalogSerDeInfo){
     SerDeInfo hiveSerDeInfo = new SerDeInfo();
+    if (catalogSerDeInfo == null) {
+      hiveSerDeInfo.setName("");
+      hiveSerDeInfo.setParameters(Maps.<String, String>newHashMap());
+      hiveSerDeInfo.setSerializationLib("org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe");
+      return hiveSerDeInfo;
+    }
     hiveSerDeInfo.setName(catalogSerDeInfo.getName());
     hiveSerDeInfo.setParameters(firstNonNull(catalogSerDeInfo.getParameters(), Maps.<String, String>newHashMap()));
     hiveSerDeInfo.setSerializationLib(catalogSerDeInfo.getSerializationLibrary());
