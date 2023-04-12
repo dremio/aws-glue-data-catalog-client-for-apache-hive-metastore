d1bae3dbeff31fce320888df58e6b90962b2bb27
Adam Szita <adam.szita@dremio.com>
2023-04-12 14:41:17 +0200

    squashed patch of hive3 changes
    Change-Id: I2964371235473ac3dab52630d4d69485ff6edd97

--- shims/loader/src/main/java/com/amazonaws/glue/shims/ShimsLoader.java
+++ shims/loader/src/main/java/com/amazonaws/glue/shims/ShimsLoader.java
@@ -16,18 +16,11 @@ public final class ShimsLoader {
 
   private static AwsGlueHiveShims loadHiveShims() {
     String hiveVersion = HiveVersionInfo.getShortVersion();
-    if (AwsGlueSparkHiveShims.supportsVersion(hiveVersion)) {
-      try {
-        return AwsGlueSparkHiveShims.class.newInstance();
-      } catch (InstantiationException | IllegalAccessException e) {
-        throw new RuntimeException("unable to get instance of Hive 1.x shim class");
-      }
-    } else {
-      try {
-        return AwsGlueHive2Shims.class.newInstance();
-      } catch (InstantiationException | IllegalAccessException e) {
-        throw new RuntimeException("unable to get instance of Hive 2.x shim class");
-      }
+
+    try {
+      return AwsGlueHive3Shims.class.newInstance();
+    } catch (InstantiationException | IllegalAccessException e) {
+      throw new RuntimeException("unable to get instance of Hive 3.x shim class for Hive version: " + hiveVersion);
     }
   }
 
